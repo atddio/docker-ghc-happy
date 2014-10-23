e50e32315351e4ce0c6b7c029bb35f32b8f5bbd6
Willem van den Ende <willem@qwan.eu>
2014-10-23 14:07:47 +0100

    fixed build haskell tools script

--- x2go-tools/scripts/build-executables-in-sandbox.sh
+++ x2go-tools/scripts/build-executables-in-sandbox.sh
@@ -5,17 +5,21 @@ set -u # exit on initialized variable
 
 export LANG="en_US.UTF-8"
 export PATH=/opt/cabal/1.20/bin:/opt/ghc/7.8.3/bin:$PATH
-export BINARIES=haskell-devtools
+export BINARIES=$(pwd)/haskell-devtools
 export packages=$1
 
 #mv ~/.cabal/bin/* /usr/local/bin
-cabal update
 mkdir -p devtools-sandbox
+mkdir -p $BINARIES
+echo "bla bla" > $BINARIES/bla #fail early
+cat $BINARIES/bla
+ls -la $BINARIES
 cd devtools-sandbox
 
+cabal update
 cabal sandbox init
-cabal install $packages 
-mv .cabal-sandbox/bin/* $BINARIES
+cabal install $packages
+mv .cabal-sandbox/bin/* "../$BINARIES/"
 
 rm -rf .cabal-sandbox
 
